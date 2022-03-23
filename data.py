1bf3792c77310247d9495505d773d536c681a663
johnyu1234 <johnyu@hotmail.my>
2022-03-23 23:18:18 +0800

    able to run exogenous variables

--- data.py
+++ data.py
@@ -3,11 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 import requests
+from statsmodels.tsa.arima.model import ARIMA
+
 url = 'https://data.taipower.com.tw/opendata/apply/file/d006005/台灣電力公司_過去電力供需資訊.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
-print(df)
-
-plt.figure(figsize=(16,5))
-plt.plot(df.index,df['備轉容量(MW)'])
-plt.show()
+x = df.iloc[:,0]
+y = df.iloc[:,3]
+j = df.iloc[:,4]
+train = y[:400]
+test = y[400:]
+train_1 = j[:400]
+test_1 = j[400:]
+for i in range(24):
+    model = ARIMA(train,exog=train_1,order=(5,1,0))
+    model_fit = model.fit()
+    # print(model_fit.summary())
+    output = model_fit.forecast(exog=test_1.iloc[i])
+    prediction = pd.Series(output.values)
+    print(prediction[0],test.iloc[i])
+    train = pd.concat([train,pd.Series(test.iloc[i])],ignore_index=True)
+    train_1 = pd.concat([train_1,pd.Series(test_1.iloc[i])],ignore_index=True)
+print(len(y))
