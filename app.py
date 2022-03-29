105128356a07b2f35adec7bd79a2e403bf280718
johnyu1234 <johnyu@hotmail.my>
2022-03-29 17:23:30 +0800

    final code

---
+++ app.py
@@ -0,0 +1,82 @@
+from statsmodels.tsa.arima.model import ARIMA
+from pmdarima.arima import auto_arima
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def NormalizeData(data):
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
+def check_stationarity(ts):
+    dftest = adfuller(ts)
+    adf = dftest[0]
+    pvalue = dftest[1]
+    critical_value = dftest[4]['5%']
+    if (pvalue < 0.05) and (adf < critical_value):
+        print('The series is stationary')
+    else:
+        print('The series is NOT stationary')
+
+
+# You can write code above the if-main block.
+if __name__ == '__main__':
+    # You should not modify this part, but additional arguments are allowed.
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--training',
+                       default='training_data.csv',
+                       help='input training data file name')
+
+    parser.add_argument('--output',
+                        default='submission.csv',
+                        help='output file name')
+    args = parser.parse_args()
+    
+    # The following part is an example.
+    # You can modify it at will.
+    import pandas as pd
+    df_training = pd.read_csv(args.training)
+    df_training['日期'] = pd.to_datetime(df_training['日期'], format='%Y-%m-%d')
+    df_training.set_index('日期', inplace=True)
+    # result = seasonal_decompose(df['備轉容量(MW)'], model='additive',extrapolate_trend='freq')
+
+    # smodel = auto_arima(train, start_p=1, start_q=1,
+    #                          test='adf',
+    #                          max_p=7, max_q=7, m=7,
+    #                          start_P=0, seasonal=True,
+    #                          d=None, D=1, trace=True,
+    #                          error_action='ignore',  
+    #                          suppress_warnings=True,
+    #                          enforce_stationarity=False,
+    #                          enforce_invertibility=False,
+    #                          stepwise=True)
+    # smodel.summary()
+    model = ARIMA(df_training,order=(2, 0, 1) ,seasonal_order=(6,1,6,7), enforce_stationarity=False, enforce_invertibility=False)
+    model_fit = model.fit()
+    # print(model_fit.summary())
+    df_result = model_fit.predict(start='2022-03-30', end='2022-4-13')
+
+    # conversion of series to dataframe
+    df_result = pd.DataFrame(df_result).reset_index(drop=False)
+    df_result.columns = ['日期','備轉容量(MW)']
+
+    # graph drawing 
+    # plt.figure(figsize = (10,6))
+    # plt.plot(df_training['備轉容量(MW)'],label="previous data",color="blue")
+    # plt.plot(test, label = "actual data", color = "green") 
+    # plt.plot(df_result['備轉容量(MW)'],label = "prediction", color='red')
+    # plt.title("ARIMA Model", size = 14)
+    # plt.show()
+
+    df_result['備轉容量(MW)'] = df_result['備轉容量(MW)'].round().astype('Int64')
+    df_result['日期'] = df_result['日期'].apply(str).str.replace('-','')
+    df_result['日期'] = df_result['日期'].str[:7]
+    df_result.to_csv(args.output,index=False)
+
+
+    # print(mean_squared_error(test,predicted,squared=False)) # RSME for evaluation of model
+
--- data.py
+++ data.py
@@ -1,62 +1,26 @@
 import csv
 import pandas as pd 
-import matplotlib.pyplot as plt
 import io
 import requests
-from statsmodels.tsa.arima.model import ARIMA
-from pmdarima.arima import auto_arima
-from sklearn.metrics import mean_squared_error
-from sklearn import preprocessing
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-import numpy as np
+
 url = 'https://data.taipower.com.tw/opendata/apply/file/d006005/台灣電力公司_過去電力供需資訊.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
-def NormalizeData(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
-def check_stationarity(ts):
-    dftest = adfuller(ts)
-    adf = dftest[0]
-    pvalue = dftest[1]
-    critical_value = dftest[4]['5%']
-    if (pvalue < 0.05) and (adf < critical_value):
-        print('The series is stationary')
-    else:
-        print('The series is NOT stationary')
-
-x = df.iloc[:,0]
-y = df.iloc[:,3]
-# y = NormalizeData(y)
-j = df.iloc[:,4]
-train = y[:-14]
-test = y[-14:]
+url_1 = 'https://data.taipower.com.tw/opendata/apply/file/d006002/本年度每日尖峰備轉容量率.csv'
+s=requests.get(url_1).content
+df_month = pd.read_csv(io.StringIO(s.decode('utf-8')))
+df_month = df_month.rename(columns={'備轉容量(萬瓩)':'備轉容量(MW)'}) #change name to MW
+df_month = df_month[['日期', '備轉容量(MW)']]
+df_month['備轉容量(MW)'] = df_month['備轉容量(MW)'].multiply(10) # converting data to MW value
+df = df[['日期', '備轉容量(MW)']]
 df['日期'] = pd.to_datetime(df['日期'], format='%Y%m%d')
 df.set_index('日期', inplace=True)
-result = seasonal_decompose(df['備轉容量(MW)'], model='additive',extrapolate_trend='freq')
-seasonal = result.seasonal
+df_month['日期'] = pd.to_datetime(df_month['日期'], format='%Y/%m/%d')
+df_month.set_index('日期', inplace=True)
 
-check_stationarity(df['備轉容量(MW)'])
-# pacf graph
-# plot_pacf(seasonal, lags =7)
-# plt.show()
-# # acf graph 
-# plot_acf(seasonal, lags =6)
-# plt.show()
-
-model = ARIMA(train,order=(1, 1, 1) ,seasonal_order=(6,1,6,7), enforce_stationarity=False, enforce_invertibility=False)
-# model = auto_arima(train,trace=True,seasonal=True,error_action='ignore', suppress_warnings=True,enforce_stationarity=False, enforce_invertibility=False)
-model_fit = model.fit()
-# print(model_fit.summary())
-predicted = model_fit.predict(start=len(train), end=len(train)+13)
-
-print(mean_squared_error(test,predicted,squared=False)) # RSME
-plt.figure(figsize = (10,6))
-plt.plot(train,label="previous data",color="blue")
-plt.plot(test, label = "true values", color = "cornflowerblue")
-plt.plot(predicted,label = "forecasts", color='darkorange')
-plt.title("ARIMA Model", size = 14)
-plt.show()
-
+df = pd.concat([df,df_month])
+index = df.index
+is_dupli = index.duplicated(keep="first")
+not_dupli = ~is_dupli
+df = df[not_dupli]
+df.to_csv("training.csv")
